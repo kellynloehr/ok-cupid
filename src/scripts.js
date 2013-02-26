ce0bf88fb5eb4dbf4e97cc1342fe354604bad377
kellynloehr <kellynloehr1@gmail.com>
2013-02-26 13:47:19 -0800

    Second Commit

---
+++ src/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("body").queryLoader2({
+        barColor: "#EEE",
+        backgroundColor: "#3F3E54",
+        percentage: true,
+        barHeight: 10,
+        completeAnimation: "grow",
+        minimumTime: 100
+    });
+});
